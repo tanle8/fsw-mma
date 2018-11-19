d5f4e7933182c5d7b232b7afed6675cff11fb6c1
Tan Le <tanld.ce@gmail.com>
2018-11-19 23:02:08 +0700

    Node HTTP Example 1

---
+++ 5-ssd_nem/week1/3-Node_HTTP.md
@@ -0,0 +1,16 @@
+# Node and HTTP
+
+## Objectives and Outcomes
+
+In this lesson you will learn more about the Node `HTTP` core module. You will:
+
+- __Create__ a simple _HTTP server_ using the Node `HTTP` module and __serve__ `HTML files` from a public folder.
+- Learn about the `fs` and `path` _Node core modules_.
+
+At the end of this lesson, you will be able to:
+
+- Create a simple HTTP server using the Node HTTP core module
+- Create a web server to serve static HTML files from a folder
+
+
+
---
+++ 5-ssd_nem/week1/node-http/index.js
@@ -0,0 +1,21 @@
+const http = require('http');   // Using HTTP core module
+
+const hostname = 'localhost';
+const port = 3000;
+
+// Setup a server
+// require parameter is the incoming HTTP request to the server
+const server = http.createServer((require, response) => {
+    // the request object give us access to the header of the 
+    // incoming HTTP request
+    console.log(require.headers);
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    response.end('<html><body><h1>Hello, World!</h1></body></html>  ');
+});
+
+// Start our server
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
